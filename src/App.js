7b403746ed06f03e3129522fe9d0fe984308f56f
Maxime <mbordas@universalflower.com>
2019-07-12 14:09:20 +0200

    api working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HydraAdmin } from '@api-platform/admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default () => <HydraAdmin entrypoint="http://127.0.0.1:8000/api"/>; // Replace with your own API entrypoint
